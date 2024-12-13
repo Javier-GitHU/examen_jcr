5b8c1d74965a1dc2d028c69a43340063212c02b9
Chavier <javiercastrorodriguez2005@gmail.com>
2024-12-13 09:17:50 +0100

    menuoption añadido

--- lib/Screens/List_view_Screen_JCR.dart
+++ lib/Screens/List_view_Screen_JCR.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
-class ListViewScreen extends StatelessWidget {
+class ListViewScreen_JCR extends StatelessWidget {
    
-  const ListViewScreen({Key? key}) : super(key: key);
+  const ListViewScreen_JCR({Key? key}) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('ListViewScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter App"),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://as01.epimg.net/meristation/imagenes/2013/09/17/noticia/1379397600_125748_1532601596_portada_normal.jpg'),
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+         children: [
+          
+         ],
       ),
     );
   }
